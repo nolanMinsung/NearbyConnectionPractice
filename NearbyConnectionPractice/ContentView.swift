537cb9f40c6f5eec467bde30a66c6b37da52bcb0
Minsung Kim <mskim4048@naver.com>
2025-11-04 19:53:29 +0900

    프로젝트 기본 파일 추가

---
+++ NearbyConnectionPractice/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  NearbyConnectionPractice
+//
+//  Created by 김민성 on 11/4/25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ NearbyConnectionPractice/NearbyConnectionPracticeApp.swift
@@ -0,0 +1,17 @@
+//
+//  NearbyConnectionPracticeApp.swift
+//  NearbyConnectionPractice
+//
+//  Created by 김민성 on 11/4/25.
+//
+
+import SwiftUI
+
+@main
+struct NearbyConnectionPracticeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
